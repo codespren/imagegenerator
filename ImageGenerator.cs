aa049be1a234e5cccd456ac55facab33618399a1
codespren <jukkanen@gmail.com>
2024-07-12 15:40:50 +0300

    Image generator v 1.0

---
+++ ImageGenerator.cs
@@ -0,0 +1,81 @@
+﻿namespace ImageGenerator {
+	using System.IO;
+	using SkiaSharp;
+
+
+/***/
+public class ImageGenerator {
+
+	private static int imageWidth = 1000;
+	private Random random;
+
+	private ulong folderNo = 0;
+	private ulong imageNo = 0;
+
+
+	/***/
+	public static void Main(string[] args) {
+		if(args.Length > 0 && args[0].Equals("all")) {
+			new ImageGenerator(true);
+		}
+		else if(args.Length > 0 && args[0].Equals("test")) {
+			new ImageGenerator(false);
+		}
+		else {
+			Console.WriteLine("Run with a parameter 'all' to create all images or with 'test' to create 10 test images.");
+		}
+    }
+
+
+	/***/
+	public ImageGenerator(bool createAll) {
+		random = new Random();
+
+		if(createAll) {
+			while(true) {
+				createImage();
+			}
+		}
+		else {
+			for(int i = 0; i < 10; i++) {
+				createImage();
+			}
+		}
+	}
+
+
+	/***/
+	private void createImage() {
+		SKBitmap bitmap = new SKBitmap(imageWidth, imageWidth);
+    	using SKCanvas canvas = new SKCanvas(bitmap);
+    	
+		SKPoint point = new SKPoint();
+		byte[] rgb = new byte[3];
+
+		for(int i = 0; i < imageWidth; i++) {
+			for(int j = 0; j < imageWidth; j++) {
+				point.X = i;
+				point.Y = j;
+
+				random.NextBytes(rgb);
+				SKColor color = new SKColor(rgb[0], rgb[1], rgb[2]);
+
+				canvas.DrawPoint(point, color);
+			}
+		}
+
+		Directory.CreateDirectory("" + folderNo);
+		string imageName = folderNo + "/" + imageNo + ".png";
+		imageNo++;
+
+		if(imageNo == ulong.MaxValue) {
+			folderNo++;
+			imageNo = 0;
+		}
+
+		using FileStream fileStream = new FileStream(imageName, FileMode.Create, FileAccess.Write);
+    	using SKImage image = SKImage.FromBitmap(bitmap); 
+    	using SKData data = image.Encode(); 
+    	data.SaveTo(fileStream);
+	}
+}}
--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# imagegenerator
+Image generator
+
+Generates all 1000 * 1000 pixel images. Images are created in a random order and there may be duplicates but eventually
+all possible images are created.
+
+The program has some interesting features, for example among the images generated are the following images:
+
+- Image of this program's source code.
+- Photo of the murder of Julius Caesar.
+- Image of next week's lottery numbers.
+- Every frame of every movie ever filmed in the past and in the future.
+- Photo of you reading this file.
+- Photo of the alien lifeform that is living closest to us.
+
+Usage:
+
+Build:
+dotnet build
+
+Create 10 test images:
+dotnet run test
+
+Create all images (make sure you have enough disk space):
+dotnet run all
+
+The program will create a folder named '0' and create first 18446744073709551615 images in to that folder.
+After that, it will create a second folder named '1' and create next 18446744073709551615 images to that folder and so on.
+When there are 18446744073709551616 folders created with 18446744073709551615 images in each it will create next images
+again to the folder named '0'. Because of that it is necessary from time to time to make a backup of older folders
+because images in them will be overwritten. However, if the computer creates for example 10 images in a second then
+the first override will not happen until 1079028307080601418780064432028 years has passed so there's no need for hurry.
